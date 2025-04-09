e1f24a704a70ad1c7bbc5c86c3e2d0324bea2e4d
a1171712023 <a117171@hotmail.com>
2025-04-09 21:03:42 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -898,7 +898,7 @@ ${displayHtml}
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Cloudflare-workers/pages-\u0076\u006c\u0065\u0073\u0073代理脚本 V24.12.13</h1>
+            <h1>Cloudflare-workers/pages-\u0076\u006c\u0065\u0073\u0073代理脚本 V25.4.7</h1>
 	    <hr>
             <p>${noteshow}</p>
             <hr>
@@ -1047,7 +1047,7 @@ ${displayHtml}
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Cloudflare-workers/pages-\u0076\u006c\u0065\u0073\u0073代理脚本 V24.12.13</h1>
+            <h1>Cloudflare-workers/pages-\u0076\u006c\u0065\u0073\u0073代理脚本 V25.4.7</h1>
 			<hr>
             <p>${noteshow}</p>
             <hr>
