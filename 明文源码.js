2e5fbbb5b055753c43ec073d2c493d8f7bf673aa
a117171js <a117171@hotmail.com>
2025-07-07 21:16:45 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -7,7 +7,7 @@ import { connect } from "cloudflare:sockets";
 let userID = "58c95ee1-1320-439f-b41b-083d4962fa59";
 
 const proxyIPs = ["us.cfpyip.dpdns.org"];
-const cn_hostnames = [''];
+const cn_hostnames = ["example.com"];
 let CDNIP = '\u0077\u0077\u0077\u002e\u0076\u0069\u0073\u0061\u002e\u0063\u006f\u006d\u002e\u0073\u0067'
 // http_ip
 let IP1 = '\u0077\u0077\u0077\u002e\u0076\u0069\u0073\u0061\u002e\u0063\u006f\u006d'
